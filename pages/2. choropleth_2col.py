5765bfa43f5f159a7795f2d8e125bcbec47ccf08
NRCAN_NingLiu <ning.liu@nrcan-rncan.gc.ca>
2023-09-22 11:12:11 -0400

    added interactive map to choropleth

--- pages/2. choropleth_2col.py
+++ pages/2. choropleth_2col.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+import leafmap.foliumap as leafmap
 import streamlit as st
 import geopandas as gpd
 # import seaborn as sns
@@ -7,7 +7,8 @@ import pandas as pd
 #layout setting
 st.set_page_config(layout="wide")
 #Load your grids shapefile data
-grids = gpd.read_file('input/gridsGEO_m.shp', driver='ESRI Shapefile')
+grids = gpd.read_file('input/gridsGEO.shp', driver='ESRI Shapefile')
+center = [55.83, -113.68]
 
 # join three column data
 def join_geom(file, geom = grids, **kwargs):
@@ -27,10 +28,10 @@ with col1:
     # define color scheme
     colorScheme = st.selectbox('Select color palette', ('viridis_r','plasma_r', 'inferno_r', 'magma_r', 'cividis_r', 'RdYlGn_r'))
     # define color scheme
-    classScheme = st.selectbox('Select classification scheme', ('natural_breaks', 'equal_interval', 'fisher_jenks', 
-                                                                # 'fisher_jenks_sampled', 'headtail_breaks', 'jenks_caspall', 
-                                                                # 'jenks_caspall_forced', 'jenks_caspall_sampled', 'max_p_classifier', 
-                                                                'maximum_breaks', 'quantiles', 'std_mean', 'percentiles'))
+    classScheme = st.selectbox('Select classification scheme', ('FisherJenks', 'BoxPlot', 'EqualInterval',  
+                                                                # 'JenksCaspall', 'JenksCaspallForced', 'JenksCaspallSampled', 
+                                                                # 'MaxP', 'MaximumBreaks', 'HeadTailBreaks', 'FisherJenksSampled',
+                                                                'NaturalBreaks', 'Quantiles', 'Percentiles', 'StdMean'))
 
     numClass = st.selectbox('Select number of classes', (5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15))
 
@@ -40,21 +41,21 @@ with col2:
     if confirmButton:
         tab1, tab2 = st.tabs(['plot', 'raw data'])
         with tab1:
-            fig, ax = plt.subplots(1, 1, figsize=(4, 4))
-            data.plot(ax=ax,
-                column="value",  # Data to plot
-                scheme=classScheme,  # Classification scheme
-                cmap=colorScheme,  # Color palette
+            m = leafmap.Map(
+                location=center, 
+                width=600,
+                height=600,
+                draw_control=False,
+                measure_control=False,
+                fullscreen_control=False)
+            m.add_data(data, 
+                column='value',
+                scheme=classScheme,
+                cmap=colorScheme,
                 k=numClass,
-                legend=True,  # Add legend
-                legend_kwds={"loc": "center left", "bbox_to_anchor": (1, 0.5)},
-            )
-            plt.axis('off')
-            # plt.legend(fontsize=4) # plt.legend(['Legend'], loc='upper left')
-            plt.xlim(-1300000, -800000)
-            plt.ylim(7290000, 7850000)
-            # plt.title(uploaded_file.name)
-            st.pyplot(fig, use_container_width=True)
+                legend_title=uploaded_file.name,
+                layer_name=uploaded_file.name)
+            m.to_streamlit(height=700)
             
         with tab2:
             st.write(data)
